5b215a1a604fa3f106da239cef2760d8f72cb7d6
LuletterSoul <qq313700046@icloud.com>
2017-09-03 22:37:34 +0800

    Initial commit

---
+++ src/components/transition/SlideUpBigTransition.vue
@@ -0,0 +1,88 @@
+<template>
+    <div id="slideUpBig">
+      <transition ：class="false"
+                  @before-enter="beforeEnter"
+                  @enter="enter"
+                  @after-enter="afterEnter"
+                  @before-leave="beforeLeave"
+                  @leave="leave"
+                  @after-leave="afterLeave"
+                  @before-appear="beforeAppear"
+                  @appear="appear">
+        <slot></slot>
+      </transition>
+    </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    name: 'SlideUpBig',
+    props: {
+      fadeInDuration: {
+        type: Number,
+        default: 1200
+      },
+      fadeInDelay: {
+        type: Number,
+        default: 300
+    },
+      fadeOutDelay: {
+        type: Number,
+        default: 500
+      },
+
+    },
+    data() {
+      return {}
+    },
+    methods: {
+      beforeEnter: function(el){
+      },
+      enter: function(el,done){
+        let vm = this;
+        Velocity(el, "transition.slideUpBigIn",
+                      { duration:vm.fadeInDuration,
+                        delay : vm.fadeInDelay,
+                        complete:function () {
+                          vm.$emit('sileInEnd');
+                          console.log("Commit current transition component.");
+                        }
+                      },done);
+      },
+      afterEnter: function (el) {
+        this.$emit('sileInEnd');
+      },
+      beforeLeave:function (el){
+        let vm = this;
+      },
+      leave:function(el,done) {
+        let vm = this;
+        Velocity(el, "transition.slideUpBigOut"
+          ,{
+              duration: vm.fadeOutDuration,
+              delay: vm.fadeOutDelay,
+              complete:function () {
+                vm.$emit('slideOutEnd');
+                console.log("Commit slide out end event to component.");
+              }
+            },
+          done
+        );
+      },
+      afterLeave: function (el) {
+//        this.$emit('slideOutEnd');
+      },
+      beforeAppear:function(el){
+        el.style = 'display:none;';
+      },
+      appear:function (el,done) {
+        let vm = this;
+        Velocity(el, "transition.slideUpBigIn",
+          { duration:vm.fadeInDuration,
+            delay : vm.fadeInDelay},done);
+      }
+    }
+  };
+</script>
+<style>
+</style>
