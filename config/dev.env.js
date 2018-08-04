1e4a9ebabf04c418dbfe4ac77f4e9e6d972450a5
roroko <372561981@qq.com>
2018-08-04 16:09:20 +0800

    API修改

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
   // BASE_API: '" https://easy-mock.com/mock/59ad3f52e0dc6633419c18f5/dm-admin"'
-  BASE_API: '"http://localhost:8085"',
+  BASE_API: '"http://118.24.62.94:8080"',
   API_VERSION:"/api/v1",
-  SERVER_API:'"http://localhost:8085/api/v1"'
+  SERVER_API:'"http://118.24.62.94:8080/api/v1"'
 });
