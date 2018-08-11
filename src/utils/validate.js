99a3da60996360920571e6bace191c19fa2b9156
XiangDe <qq313700046@icloud.com>
2018-08-11 20:12:56 +0800

    校验规则

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -8,6 +8,28 @@ export function isWscnEmail(str) {
   return reg.test(str.trim());
 }
 
+//用户名正则，4到16位（字母，数字，下划线，减号）
+export function validateUsername(username) {
+  const uPattern = /^[a-zA-Z0-9_-]{4,16}$/;
+  return uPattern.test(username.trim());
+}
+
+//校验密码：只能输入6-20个字母、数字、下划线
+export function validatePass(password) {
+  const pPattern=/^(\w){6,20}$/;
+  return pPattern.test(password.trim());
+}
+
+export function validateStuId(id) {
+  const idPattern = /^[A-Za-z0-9]+$/;
+  return idPattern.test(id.trim());
+}
+
+export function validateZh(name) {
+  const cnPattern = /[\u4E00-\u9FA5]/;
+  return cnPattern.test(name.trim());
+}
+
 /* 合法uri*/
 export function validateURL(textval) {
   const urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
