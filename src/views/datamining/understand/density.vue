1573720292383a0f6671dded73e424617d171c96
XiangDe <qq313700046@icloud.com>
2018-08-15 17:11:04 +0800

    增加加载Loading

--- src/views/datamining/understand/density.vue
+++ src/views/datamining/understand/density.vue
@@ -21,6 +21,25 @@
             </el-col>
           </el-row>
         </div>
+        <div class="section">
+          <el-row style="margin-top: 20px">
+            <el-col :offset="20" :span="3">
+              <el-select size="medium"
+                         clearable
+                         v-model="xName"
+                         placeholder="排序">
+                <el-option v-for="item in xOptions" :key="item.key" :label="item.label" :value="item.key">
+                </el-option>
+              </el-select>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col>
+              <scatter :height="'960px'" :option="_boxOption">
+              </scatter>
+            </el-col>
+          </el-row>
+        </div>
       </full-page>
     </el-row>
   </div>
@@ -29,8 +48,20 @@
 <script>
   import Scatter from '@/components/Charts/scatter'
   import EchartStat from 'echarts-stat'
+  import dataTool from 'echarts/dist/extension/dataTool'
+  import { Loading } from 'element-ui';
   import echarts from 'echarts';
 
+  var CATEGORY_DIM_COUNT = 6;
+  var GAP = 1;
+  var BASE_LEFT = 5;
+  var BASE_TOP = 10;
+  // var GRID_WIDTH = 220;
+  // var GRID_HEIGHT = 220;
+  var GRID_WIDTH = (100 - BASE_LEFT - GAP) / CATEGORY_DIM_COUNT - GAP;
+  var GRID_HEIGHT = (100 - BASE_TOP - GAP) / CATEGORY_DIM_COUNT - GAP;
+  var CATEGORY_DIM = 7;
+  var SYMBOL_SIZE = 3;
   export default {
     name: "density",
     components: {
@@ -41,6 +72,26 @@
         type: String,
         default: 'Linear Regression'
       }
+    },
+    beforeRouteEnter (to, from, next) {
+      Loading.service({
+        lock: true,
+        text: '正在生成视图,请稍后...',
+        spinner: 'el-icon-loading',
+        background: 'rgba(0, 0, 0, 0.7)'
+      });
+      next(vm => {})
+    },
+    mounted: function () {
+      let vm = this;
+      this.$nextTick(() => { // 以服务的方式调用的 Loading 需要异步关闭
+        Loading.service({
+          lock: true,
+          text: '正在生成视图,请稍后...',
+          spinner: 'el-icon-loading',
+          background: 'rgba(0, 0, 0, 0.7)'
+        }).close();
+      });
     },
     data() {
       return {
@@ -86,7 +137,18 @@
         girth: [8.3, 8.6, 8.8, 10.5, 10.7, 10.8, 11.0, 11.0, 11.1, 11.2, 11.3, 11.4, 11.4, 11.7, 12.0, 12.9, 12.9, 13.3, 13.7, 13.8, 14.0, 14.2, 14.5, 16.0, 16.3, 17.3, 17.5, 17.9, 18.0, 18.0, 20.6]
       }
     },
-    methods: {},
+    methods: {
+      formatter(param) {
+        return [
+          'Experiment ' + param.name + ': ',
+          'upper: ' + param.data[0],
+          'Q1: ' + param.data[1],
+          'median: ' + param.data[2],
+          'Q3: ' + param.data[3],
+          'lower: ' + param.data[4]
+        ].join('<br/>')
+      }
+    },
     computed: {
       _histogramReg() {
         let regression = EchartStat.histogram(this.girth);
@@ -128,6 +190,130 @@
             data: this._histogramReg.data
           }]
         }
+      },
+      _boxOption() {
+        return {
+          title: {
+            text: 'Multiple Categories',
+            left: 'center',
+          },
+          legend: {
+            y: '10%',
+            data: ['category0', 'category1', 'category2', 'category3']
+          },
+          tooltip: {
+            trigger: 'item',
+            axisPointer: {
+              type: 'shadow'
+            }
+          },
+          grid: {
+            left: '10%',
+            top: '20%',
+            right: '10%',
+            bottom: '15%'
+          },
+          xAxis: {
+            type: 'category',
+            data: this._boxData[0].axisData,
+            boundaryGap: true,
+            nameGap: 30,
+            splitArea: {
+              show: true
+            },
+            axisLabel: {
+              formatter: 'expr {value}'
+            },
+            splitLine: {
+              show: false
+            }
+          },
+          yAxis: {
+            type: 'value',
+            name: 'Value',
+            min: -400,
+            max: 600,
+            splitArea: {
+              show: false
+            }
+          },
+          dataZoom: [
+            {
+              type: 'inside',
+              start: 0,
+              end: 20
+            },
+            {
+              show: true,
+              height: 20,
+              type: 'slider',
+              top: '90%',
+              xAxisIndex: [0],
+              start: 0,
+              end: 20
+            }
+          ],
+          series: [
+            {
+              name: 'category0',
+              type: 'boxplot',
+              data: this._boxData[0].boxData,
+              tooltip: {formatter: this.formatter}
+            },
+            {
+              name: 'category1',
+              type: 'boxplot',
+              data: this._boxData[1].boxData,
+              tooltip: {formatter: this.formatter}
+            },
+            {
+              name: 'category2',
+              type: 'boxplot',
+              data: this._boxData[2].boxData,
+              tooltip: {formatter: this.formatter}
+            },
+            {
+              name: 'outlier',
+              type: 'pictorialBar',
+              symbolPosition: 'end',
+              symbolSize: 8,
+              data: this._boxData[0].outliers
+            },
+            {
+              name: 'outlier',
+              type: 'pictorialBar',
+              symbolPosition: 'end',
+              symbolSize: 8,
+              data: this._boxData[1].outliers
+            },
+            {
+              name: 'outlier',
+              type: 'pictorialBar',
+              symbolPosition: 'end',
+              symbolSize: 8,
+              barGap: '30%',
+              data: this._boxData[2].outliers
+            }
+          ]
+        }
+      },
+      _boxData() {
+        let data = [];
+        for (let seriesIndex = 0; seriesIndex < 5; seriesIndex++) {
+          let seriesData = [];
+          for (let i = 0; i < 18; i++) {
+            let cate = [];
+            for (let j = 0; j < 90; j++) {
+              cate.push(Math.random() * 200);
+            }
+            for (let j = 0; j < 10; j++) {
+              cate.push(Math.random() * 400);
+            }
+            seriesData.push(cate);
+          }
+          data.push(dataTool.prepareBoxplotData(seriesData));
+        }
+        return data;
       }
     }
   }
--- src/views/datamining/understand/regression.vue
+++ src/views/datamining/understand/regression.vue
@@ -97,11 +97,32 @@
   import Scatter from '@/components/Charts/scatter'
   import EchartStat from 'echarts-stat'
   import echarts from 'echarts';
+  import { Loading } from 'element-ui';
 
   export default {
     name: "regression",
     components: {
       Scatter
+    },
+    beforeRouteEnter (to, from, next) {
+      Loading.service({
+        lock: true,
+        text: '正在生成视图,请稍后...',
+        spinner: 'el-icon-loading',
+        background: 'rgba(0, 0, 0, 0.7)'
+      });
+      next(vm => {})
+    },
+    mounted: function () {
+      let vm = this;
+      this.$nextTick(() => { // 以服务的方式调用的 Loading 需要异步关闭
+        Loading.service({
+          lock: true,
+          text: '正在生成视图,请稍后...',
+          spinner: 'el-icon-loading',
+          background: 'rgba(0, 0, 0, 0.7)'
+        }).close();
+      });
     },
     props: {
       setName: {
