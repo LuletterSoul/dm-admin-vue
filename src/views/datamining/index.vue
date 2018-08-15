27b92f5edd8dba022378a94dc09192d2ec12262c
XiangDe <qq313700046@icloud.com>
2018-08-15 22:40:07 +0800

    新增数据导入页面

--- src/views/datamining/index.vue
+++ src/views/datamining/index.vue
@@ -1,24 +1,25 @@
 <template>
-    <div>
-      <transition name="fade" mode="out-in">
-        <keep-alive :include='cachedViews'>
-          <router-view></router-view>
-        </keep-alive>
-      </transition>
-    </div>
+  <div>
+    <transition name="fade" mode="out-in">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
+  </div>
 </template>
 
 <script>
-    export default {
-      name: "index",
-      data() {
-        return {
-          cachedViews() {
-            return this.$store.state.tagsView.cachedViews.join();
-          }
-        }
+  export default {
+    name: "index",
+    data() {
+      return {}
+    },
+    computed: {
+      _cachedViews() {
+        return this.$store.getters.cachedViews;
       }
     }
+  }
 </script>
 
 <style scoped>
