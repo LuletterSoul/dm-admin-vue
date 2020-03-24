c7d6aabe587884322b170ffb38514c3427302208
XiangDe <qq313700046@icloud.com>
2020-03-24 16:10:10 +0800

    Update prod ip

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://118.24.62.94:8081"',
-  API_VERSION:"/api/v1",
-  SERVER_API:'"http://118.24.62.94:8081/ins"'
+  BASE_API: '"http://10.196.122.94:8080"',
+  API_VERSION: "/api/v1",
+  SERVER_API: '"http://10.196.122.94:8080"'
 };
