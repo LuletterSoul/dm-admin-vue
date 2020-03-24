3c534bcf83263095185800a58d3198cd72270faf
XiangDe <qq313700046@icloud.com>
2020-03-24 17:50:58 +0800

    Update test profile

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,9 +3,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  // BASE_API: '" https://easy-mock.com/mock/59ad3f52e0dc6633419c18f5/dm-admin"'
-  BASE_API: '"http://118.24.62.94:8080"',
-  API_VERSION:"/api/v1",
-  SERVER_API:'"http://118.24.62.94:8080/api/v1"'
+  BASE_API: '"http://221.226.81.54:1211"',
+  API_VERSION: "/api/v1",
+  SERVER_API: '"http://221.226.81.54:1211"'
 });
--- config/test.env.js
+++ config/test.env.js
@@ -3,7 +3,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"test"',
-  BASE_API: '"http://221.226.81.54:1211"',
+  BASE_API: '"http://10.196.122.94:8080"',
   API_VERSION: "/api/v1",
-  SERVER_API: '"http://221.226.81.54:1211"'
+  SERVER_API: '"http://10.196.122.94:8080"'
 });
