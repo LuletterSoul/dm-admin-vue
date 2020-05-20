124c188046ca5d5c33f758570d9a0ad071c3f2fe
XiangDe <qq313700046@icloud.com>
2020-05-20 22:28:26 +0800

    Update test.env.js

--- config/test.env.js
+++ config/test.env.js
@@ -5,5 +5,5 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"test"',
   BASE_API: '"http://10.196.122.94:8080"',
   API_VERSION: "/api/v1",
-  SERVER_API: '"http://192.168.0.134:8085"'
+  SERVER_API: '"http://192.168.0.28:8085"'
 });
