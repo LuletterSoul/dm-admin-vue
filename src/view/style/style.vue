9e4fd80e751f5e810364aaea63506fad2bb9e4cd
JinShiyin <shiyinjin@foxmail.com>
2020-12-07 10:33:55 +0800

    add style.vue

--- src/view/style/style.vue
+++ src/view/style/style.vue
@@ -1,7 +1,12 @@
+<!--<script src="../../router.js"></script>-->
 <template>
     <div>
-        <image-previewer v-if="show_type==='image'" v-bind:image_info="show_info" @onChangeView="onChangeView"></image-previewer>
-        <VideoPreviewer v-if="show_type==='video'" v-bind:video_info="show_info" @onChangeView="onChangeView"></VideoPreviewer>
+        <image-previewer v-if="_showType==='photo'" v-bind:oriInfo="_oriInfo" v-bind:stylizedInfo="_stylizedInfo"
+                         v-bind:completed="_completed" v-bind:viewRes="viewRes"
+                         @onChangeView="onChangeView"></image-previewer>
+        <VideoPreviewer v-if="_showType==='video'" v-bind:oriInfo="_oriInfo" v-bind:stylizedInfo="_stylizedInfo"
+                        v-bind:completed="_completed" v-bind:viewRes="viewRes"
+                        @onChangeView="onChangeView"></VideoPreviewer>
     </div>
 </template>
 
@@ -14,29 +19,72 @@
         // components: {ImagePreviewer},
         // components: {VideoPreviewer},
         components: {ImagePreviewer, VideoPreviewer},
-
-        data() {
-            return {
-                show_type: 'video',
-                show_info: {
-                    completed: true,
-                    viewRes: false,
-                    oriInfo: {
-                        video: 'https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4',
+        props: {
+            showType: {
+                type: String,
+                default: 'photo'
+            },
+            completed: {
+                type: Boolean,
+                default: false
+            },
+            oriInfo: {
+                type: Object,
+                default: () => {
+                    return {
+                        video: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
                         thumbnail: 'https://i.loli.net/2019/06/06/5cf8c5d9c57b510947.png',
                         source: 'https://s3.ax1x.com/2020/12/04/DH4AFU.jpg'
-                    },
-                    stylizedInfo: {
+                    }
+                }
+            },
+            stylizedInfo: {
+                type: Object,
+                default: () => {
+                    return {
                         video: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
                         thumbnail: 'https://i.loli.net/2019/06/06/5cf8c5d9c57b510947.png',
                         source: 'https://s3.ax1x.com/2020/12/04/DbJAsI.png'
                     }
                 }
+            },
+        },
+        computed: {
+            _showType() {
+                return this.showType
+                // return this.$route.query.showType
+            },
+            _completed(){
+                return this.completed
+                // return this.$route.query.completed==='true'
+            },
+            _oriInfo() {
+                return this.oriInfo
+                // return this.$route.query.oriInfo
+            },
+            _stylizedInfo() {
+                return this.stylizedInfo
+                // return this.$route.query.stylizedInfo
+            },
+        },
+        data() {
+            return {
+                viewRes: false,
+                // stylizedInfo: {
+                //     video: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+                //     thumbnail: 'https://i.loli.net/2019/06/06/5cf8c5d9c57b510947.png',
+                //     source: 'https://s3.ax1x.com/2020/12/04/DbJAsI.png'
+                // },
+                // oriInfo: {
+                //     video: 'https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4',
+                //     thumbnail: 'https://i.loli.net/2019/06/06/5cf8c5d9c57b510947.png',
+                //     source: 'https://s3.ax1x.com/2020/12/04/DH4AFU.jpg'
+                // },
             }
         },
         methods: {
-            onChangeView(){
-                this.show_info.viewRes = !this.show_info.viewRes
+            onChangeView() {
+                this.viewRes = !this.viewRes
             }
         }
 
